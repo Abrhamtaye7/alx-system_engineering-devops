cf2a7553e7839994cdf32add5b6b64fc467631b8
Zuse Taye <101526849+Abrhamtaye7@users.noreply.github.com>
2022-10-14 22:21:28 +0300

    Update 1

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-# Create a file in /tmp
-
-file { 'school':
-  ensure  => 'present',
-  content => 'I love Puppet',
-  group   => 'www-data',
+# manifest to create file in tmp directory
+file { '/tmp/school':
+  ensure  => present,
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
-  path    => '/tmp/school',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
